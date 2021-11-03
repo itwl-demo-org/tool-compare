834d149bbb02758bff3a8fc9e3865cb5a0f08ec9
Javier Ruiz Jiménez <jruiz@itwonderlab.com>
2021-11-03 21:27:44 +0100

    Add tag to instance

--- test-cases/terraform/aws/iam/iam-entities/public_and_private_ec2_same_role/main.tf
+++ test-cases/terraform/aws/iam/iam-entities/public_and_private_ec2_same_role/main.tf
@@ -15,8 +15,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   cidr = "10.0.0.0/16"
   manage_default_security_group= true
@@ -100,7 +100,9 @@ resource "aws_instance" "pub_ins" {
   instance_type = "t2.micro"
   subnet_id = module.vpc.public_subnets[0]
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
-
+  tags = {
+    Name = "pub_ins"
+  }
 }
 
 resource "aws_instance" "priv_ins" {
